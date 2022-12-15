31ba50726d2d892ba6410eaf4801907139d739c7
IZATUL NABILAH <2020456092@student.uitm.edu.my>
2022-12-15 14:25:53 +0800

    newfile

--- app/src/main/java/com/example/zakatcalculatorpro/AboutActivity.java
+++ app/src/main/java/com/example/zakatcalculatorpro/AboutActivity.java
@@ -2,21 +2,35 @@ package com.example.zakatcalculatorpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.ImageView;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
 import android.view.View;
-import android.widget.TextView;
+
 
 public class AboutActivity extends AppCompatActivity {
 
-    TextView textView;
-
+    ImageView github;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aboutme);
-        textView = findViewById(R.id.textViewLink);
-        textView. setMovementMethod(LinkMovementMethod.getInstance());
+
+        github = findViewById(R.id.githubView);
+
+        github.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                gotoUrl("https://github.com/Izatul-Nabilah/Assigntment1");
+            }
+
+        });
     }
 
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
+    }
 }
