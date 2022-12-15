7b2e4ffc1ccb5b3ae50d740b9debab681b40b1be
IZATUL NABILAH <2020456092@student.uitm.edu.my>
2022-12-15 13:08:29 +0800

    NewFinals

--- app/src/main/java/com/example/zakatcalculatorpro/AboutActivity.java
+++ app/src/main/java/com/example/zakatcalculatorpro/AboutActivity.java
@@ -3,13 +3,20 @@ package com.example.zakatcalculatorpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aboutme);
-        textView
+        textView = findViewById(R.id.textViewLink);
+        textView. setMovementMethod(LinkMovementMethod.getInstance());
     }
+
 }
